8948f510cf2c8fac26112d0204280b3666afc469
Yevhen Rudofylov <rudofiloveugene@gmail.com>
2022-03-26 21:31:11 +0200

    some fixes

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 fetch("https://jsonplaceholder.typicode.com/users")            
 .then((response) => response.json())
 .then((data) => {
-    console.log(data.filter(object => object.company.name === "Johns Group" ));
+    let user = data.filter(({company:{name}}) => name === "Johns Group" );
+    console.log(user);
 })
 .catch((error) => console.error(error));
